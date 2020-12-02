673df1700457a2a512e8350f9ed8269b091c6ce7
Lulu <me@erkin.party>
2020-12-02 10:23:31 +0300

    Solve the first day

--- 2020/01.rkt
+++ 2020/01.rkt
@@ -1,11 +1,48 @@
 #lang racket
 
+(module amb racket/base
+  (provide amb assertion)
+  ;; Matthew Might's implementation
+  (define fail-stack '())
+  (define (fail)
+    (if (null? fail-stack)
+        (error 'amb "Back-tracking stack exhausted!")
+        (let ((back-track-point (car fail-stack)))
+          (set! fail-stack (cdr fail-stack))
+          (back-track-point back-track-point))))
+  (define (amb choices)
+    (let ((cc (call/cc values)))
+      (cond
+        ((null? choices) (fail))
+        ((pair? choices) (let ((choice (car choices)))
+                           (set! choices (cdr choices))
+                           (set! fail-stack (cons cc fail-stack))
+                           choice)))))
+  (define (assertion condition)
+    (when (not condition)
+      (fail))))
+
+(require 'amb)
+
 (define ((adds-up? n a) b)
   (= n (+ a b)))
 
+(define input (file->list "input01.txt" read))
+
 (define (part-1)
-  (define input (file->list "input01.txt" read))
   (let loop ((n (car input)) (lst (cdr input)))
     (cond ((findf (adds-up? 2020 n) lst) => (curry * n))
           (else (loop (car lst) (cdr lst))))))
 
+(define (part-2)
+  (let ((a (amb input))
+        (b (amb input))
+        (c (amb input)))
+    (assertion (= 2020 (+ a b c)))
+    (* a b c)))
+
+(module+ main
+  (display "Day 1, Part 1: ")
+  (time (part-1))
+  (display "Day 1, Part 2: ")
+  (time (part-2)))
