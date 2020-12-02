66d2f182d5ebb43fc7181da53e36a2f542c848ab
Lulu <me@erkin.party>
2020-12-02 11:49:43 +0300

    Solve D2P2

--- 2020/02.rkt
+++ 2020/02.rkt
@@ -13,8 +13,17 @@
         (string->number max)))
   (count valid-entry? input))
 
+(define (part-2)
+  (define (valid-entry? str)
+    (define-values (_ yes no letter password)
+      (apply values (regexp-match #rx"([0-9]+)-([0-9]+) ([a-z]):( [a-z]+)" str)))
+    (let ((positions (regexp-match-positions* letter password)))
+      (xor (assoc (string->number yes) positions)
+           (assoc (string->number no) positions))))
+  (count valid-entry? input))
+
 (module+ main
   (display "Day 2, Part 1: ")
   (time (part-1))
-  #;  (display "Day 2, Part 2: ")
-  #;  (time (part-2)))
+  (display "Day 2, Part 2: ")
+  (time (part-2)))
