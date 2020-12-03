f40e6a24632b08461ad89e35a993fb58029ae519
Lulu <me@erkin.party>
2020-12-03 11:51:30 +0300

    Solve Day 3 Part 2

--- 2020/03.rkt
+++ 2020/03.rkt
@@ -17,12 +17,32 @@
                  (add1 trees)
                  trees))))))
 
-#;
 (define (part-2)
-  )
+  (define (nth-rest lst n)
+    (if (or (zero? n) (null? lst))
+        lst
+        (nth-rest (cdr lst) (sub1 n))))
+  (define (tree-check right down)
+    (let loop ((rows input) (shift 0) (trees 0))
+      (cond
+        ((null? rows)
+         trees)
+        ((>= shift 31)
+         (loop rows (- shift 31) trees))
+        (else
+         (loop (nth-rest rows down)
+               (+ shift right)
+               (if (char=? #\# (string-ref (car rows) shift))
+                 (add1 trees)
+                 trees))))))
+  (* (tree-check 1 1)
+     (tree-check 3 1)
+     (tree-check 5 1)
+     (tree-check 7 1)
+     (tree-check 1 2)))
 
 (module+ main
   (display "Day 3, Part 1: ")
   (time (part-1))
-#;  (display "Day 3, Part 2: ")
-#;  (time (part-2)))
+  (display "Day 3, Part 2: ")
+  (time (part-2)))
